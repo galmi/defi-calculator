eb0b16aa20d813e29b1d735116d37e6615a7ea1d
galmi <galmi.ru@gmail.com>
2022-09-15 01:45:01 +0700

    fixed rootURL

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function (environment) {
   let ENV = {
     modulePrefix: 'frontend',
     environment,
-    rootURL: '/',
+    rootURL: '/defi-calculator/',
     locationType: 'history',
     EmberENV: {
       FEATURES: {
