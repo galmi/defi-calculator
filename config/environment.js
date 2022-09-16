9d77e17048915bf69e1daeb2f118229e3b393d1d
galmi <galmi.ru@gmail.com>
2022-09-16 13:05:37 +0700

    changed location to hash

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function (environment) {
     modulePrefix: 'frontend',
     environment,
     rootURL: '/defi-calculator/',
-    locationType: 'history',
+    locationType: 'hash',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
